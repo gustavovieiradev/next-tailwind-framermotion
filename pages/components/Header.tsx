3082728d4d11d824f05a74d00aaee81fb67383a5
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-06-20 18:30:17 -0300

    chore(*): removendo imports que nao esta sendo usado

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,20 +1,21 @@
-import Image from 'next/image';
 import Link from 'next/link';
 
 export function Header() {
   return (
     <header className="flex justify-center md:justify-between py-2 md:py-4 items-center">
-      <Image src="/img/logo.svg" alt="logo" width={150} height={33} />
+      <Link href="https://github.com/gustavovieiradev/next-tailwind-framermotion">
+        <a className="hover:text-gray-300 transition" target="_blank">gustavovieiradev</a>
+      </Link>
 
       <nav className="hidden md:block space-x-8">
         <Link href="nextjs">
-          <a className="tracking-wide hover:text-gray-300">Next.js</a>
+          <a className="tracking-wide hover:text-gray-300 transition">Next.js</a>
         </Link>
         <Link href="tailwind">
-          <a className="tracking-wide hover:text-gray-300">Tailwind</a>
+          <a className="tracking-wide hover:text-gray-300 transition">Tailwind</a>
         </Link>
         <Link href="framermotion">
-          <a className="tracking-wide hover:text-gray-300">Framer Motion</a>
+          <a className="tracking-wide hover:text-gray-300 transition">Framer Motion</a>
         </Link>
       </nav>
 
